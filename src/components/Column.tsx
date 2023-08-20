59c7b02862dc295bc5130df8c212901d99b629bb
Chaturved Degloorkar <48447253+chaturrved@users.noreply.github.com>
2023-08-20 21:32:07 +0530

    basic structure of columns and cards

---
+++ src/components/Column.tsx
@@ -0,0 +1,17 @@
+import { ColumnContainer, ColumnTitle, CardContainer } from "../styles";
+import { Card } from "./Card";
+
+type ColumnProps = {
+    text: string
+}
+
+export const Column = ({text}: ColumnProps) => {
+    return (
+      <ColumnContainer>
+        <ColumnTitle>{text}</ColumnTitle>
+        <Card text="Generate App Scaffold"></Card>
+        <Card text="Learn Typescript"></Card>
+        <Card text="Begin to use static typing"></Card>
+      </ColumnContainer>
+    );
+}
