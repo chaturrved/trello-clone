99f296ec4803f149950a7906b17c528c2906cb0a
Chaturved Degloorkar <48447253+chaturrved@users.noreply.github.com>
2023-08-20 23:05:38 +0530

    added form to add new item or list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { AppContainer } from "./styles";
 import { Column } from "./components/Column";
+import { AddNewItem } from "./components/AddNewItem";
 
 export const App=() => {
   return (
     <AppContainer>
       <Column text="Todo:"></Column>
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={console.log}
+      />
     </AppContainer>
   );
 }
